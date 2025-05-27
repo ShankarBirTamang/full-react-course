669b8eb84818950ef21eabdaf1dfa1e95adc9d7e
ShankarBirTamang <sankar.jt19@gmail.com>
2025-05-27 12:38:56 +0545

    ex2: jsx welcome message

---
+++ 1.Fundamentals/2.JSX/src/App.jsx
@@ -0,0 +1,9 @@
+import WelcomeMessage from "../solution/WelcomeMessage.jsx"
+const App = () => {
+  return <section id="section">
+    My Website
+    <WelcomeMessage/>
+  </section>
+};
+
+export default App;
