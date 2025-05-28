c138fac8e92806ef46318c074f1dcc695a91f8f0
ShankarBirTamang <sankar.jt19@gmail.com>
2025-05-28 11:05:51 +0545

    `Added button text state and mouse move tracking to EventDemo component`

--- 1.Fundamentals/12. Events/components/EventDemo.jsx
+++ 1.Fundamentals/12. Events/components/EventDemo.jsx
@@ -3,12 +3,22 @@ import { useState } from 'react'
 
 function EventDemo() {
     const [clicked,setClicked] = useState(false);
+    const [buttonText,setButtonText] = useState('Click the button');
+    const [mouseMoved , setMouseMoved] = useState(false);
   return (
     <>
-      <button onClick={()=>setClicked(!clicked)}>Click Here!</button>
-      { clicked  && <div onMouseMove={()=>console.log("mouse moved")}>
-          <p>"Button Clicked"</p>
+      <button onClick={()=>setClicked(!clicked)}>First Button</button>
+      { clicked  && <div 
+        onMouseMove={()=>setMouseMoved(true)}
+        onMouseLeave={()=>setMouseMoved(false)}>
+          <p style={{
+            backgroundColor : mouseMoved? 'lightyellow':'transparent',
+            padding : '10px',
+          }}>"First Button Clicked"</p>
       </div>}
+      <br/><br/>
+      <button onClick={()=>setButtonText('Second Button Clicked')}>Second button</button>
+      <p >{buttonText}</p>
     </>
   )
 }
