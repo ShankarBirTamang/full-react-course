f99ecb671c35dc1009c633f8cbc4c98bbd674b35
ShankarBirTamang <sankar.jt19@gmail.com>
2025-05-28 10:36:40 +0545

    `Added package.json file with dependencies and scripts for a React project.`

---
+++ 1.Fundamentals/12. Events/components/EventDemo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function EventDemo() {
+  return (
+    <>
+      <button>Click Here!</button>
+      <div>
+          <p>"Button Clicked"</p>
+      </div>
+    </>
+  )
+}
+
+export default EventDemo
