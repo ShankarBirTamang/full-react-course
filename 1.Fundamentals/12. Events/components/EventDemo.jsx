dcee81f97611c3300d48c473e78ba962035f3969
ShankarBirTamang <sankar.jt19@gmail.com>
2025-05-28 10:49:47 +0545

    `Added useState hook and conditional rendering based on state`

--- 1.Fundamentals/12. Events/components/EventDemo.jsx
+++ 1.Fundamentals/12. Events/components/EventDemo.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useState } from 'react'
 
 function EventDemo() {
+    const [clicked,setClicked] = useState(false);
   return (
     <>
-      <button>Click Here!</button>
-      <div>
+      <button onClick={()=>setClicked(!clicked)}>Click Here!</button>
+      { clicked  && <div onMouseMove={()=>console.log("mouse moved")}>
           <p>"Button Clicked"</p>
-      </div>
+      </div>}
     </>
   )
 }
